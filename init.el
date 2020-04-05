2f17baf35b967401e8a093a34c3c2f014c1069d3
Juri Pärn <juri.paern@gmail.com>
2020-04-05 17:19:58 +0200

    Only necessary entries in init.el.

--- init.el
+++ init.el
@@ -16,19 +16,5 @@
 ;; ~./emacs.d/config.org
 ;; To load config.org, type C-c e
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (magit flycheck smex ido-vertical-mode company zerodark-theme powerline-evil evil-org evil-indent-textobject evil-surround evil-leader evil beacon use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;;; init.el ends here
